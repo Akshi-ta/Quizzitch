6f946e01374b027c6779e993e97c269f50d944e6
Sanyam Ratreja <sanyamratreja18@gmail.com>
2022-02-05 20:16:58 +0530

    upgrade

--- app/src/main/java/com/example/quizzitch/Dash.kt
+++ app/src/main/java/com/example/quizzitch/Dash.kt
@@ -26,7 +26,7 @@ class Dash : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.profilePage
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
