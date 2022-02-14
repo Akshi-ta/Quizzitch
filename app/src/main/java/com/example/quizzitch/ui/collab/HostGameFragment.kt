e21f8e03f5fcdb2500b7c9129b2c5115cb004efb
Sanyam Ratreja <sanyamratreja18@gmail.com>
2022-02-14 19:24:56 +0530

    host game 1st draft

--- app/src/main/java/com/example/quizzitch/ui/collab/HostGameFragment.kt
+++ app/src/main/java/com/example/quizzitch/ui/collab/HostGameFragment.kt
@@ -3,7 +3,6 @@ package com.example.quizzitch.ui.collab
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,11 +11,13 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
 import com.example.quizzitch.R
 import com.google.android.material.snackbar.Snackbar
+import com.google.common.hash.HashCode
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.coroutines.joinAll
+import com.google.firebase.firestore.QuerySnapshot
 
 class HostGameFragment : Fragment() {
     val mauth = FirebaseAuth.getInstance()
@@ -33,127 +34,261 @@ class HostGameFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_host_game, container, false)
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val createRoomCard: CardView = view.findViewById(R.id.createRoomCard)
-        val createText : TextView = view.findViewById(R.id.textView28)
+        val createTextView: TextView = view.findViewById(R.id.textView28)
         val joinTextView: TextView = view.findViewById(R.id.textView29)
+        val createRoomCard: CardView = view.findViewById(R.id.createRoomCard)
         val joinRoomCard: CardView = view.findViewById(R.id.joinRoomCard)
-        joinRoomCard.visibility = View.GONE
-        createText.setOnClickListener{
-            joinRoomCard.visibility = View.GONE
-            createRoomCard.visibility = View.VISIBLE
-        }
-        joinTextView.setOnClickListener{
-            createRoomCard.visibility = View.GONE
-            joinRoomCard.visibility = View.VISIBLE
-        }
-
-        val maxPlayerEdit: EditText = view.findViewById(R.id.editTextMaxPlayers)
-        val generate: Button = view.findViewById(R.id.button5)
-        generate.setOnClickListener{
-            val a = maxPlayerEdit.text.toString()
-            val bets: String = view.findViewById<EditText>(R.id.editTextBets).text.toString()
-            if(a.isEmpty() || bets.isEmpty())
+        val generateRoom: Button = view.findViewById(R.id.button5)
+        val maxPlayers: EditText = view.findViewById(R.id.editTextMaxPlayers)
+        val bets: EditText = view.findViewById(R.id.editTextBets)
+        val leaveBt: Button = view.findViewById(R.id.leaveBt)
+        val joinBt: Button = view.findViewById(R.id.joinBt)
+        store.collection("games").document(uid).get().addOnSuccessListener {
+            if(it["anyGameActive"]==null)
             {
-                Snackbar.make(generate,"Enter All Fields First.", Snackbar.LENGTH_LONG).show()
-            }else if(a.toInt()>4)
-            {
-                Snackbar.make(generate,"Max players supported upto 4.", Snackbar.LENGTH_LONG).show()
-            }else if(bets.toInt()<100)
-            {
-                Snackbar.make(generate,"Minimum bet limit is 100", Snackbar.LENGTH_LONG).show()
+                val userGames: HashMap<String, Any> = hashMapOf()
+                userGames["anyGameActive"] = -1
+                basicCreate(view)
+                store.collection("games").document(uid).update(userGames)
             }else
             {
-
-                MakeRoom(a, bets, view)
-                generate.isClickable = false
-                joinRoomCard.visibility = View.VISIBLE
-                createRoomCard.visibility = View.GONE
-                createText.isClickable = false
+                if(it["anyGameActive"].toString().toInt()==-1)
+                {
+                    basicCreate(view)
+                }else
+                {
+                    basicJoin(view,1)
+                }
             }
-
         }
+
+        joinTextView.setOnClickListener{
+            basicJoin(view,0)
+        }
+        createTextView.setOnClickListener{
+            basicCreate(view)
+        }
+
+        generateRoom.setOnClickListener{
+            val players: String = maxPlayers.text.toString()
+            val bet: String = bets.text.toString()
+            if(players.isEmpty())
+            {
+                Toast.makeText(requireContext(), "Enter Players", Toast.LENGTH_LONG).show()
+            }else if(players.toInt()>4)
+            {
+                Toast.makeText(requireContext(), "Max Players allowed are 4", Toast.LENGTH_LONG).show()
+            }else if(bet.isEmpty()|| bet.toInt()<100)
+            {
+                Toast.makeText(requireContext(), "Bet amount cannot be less than 100", Toast.LENGTH_LONG).show()
+            }else
+            {
+                generateRoom.isClickable = false
+                createRoom(view, players, bet)
+            }
+        }
+
+        leaveBt.setOnClickListener{
+            leaveRoom()
+        }
+
+        joinBt.setOnClickListener{
+            basicJoin(view,2)
+        }
+
+
 
     }
 
-    fun MakeRoom(a: String, bets:String, view: View) {
+    private fun basicCreate(view: View){
+        val createTextView: TextView = view.findViewById(R.id.textView28)
+        val joinTextView: TextView = view.findViewById(R.id.textView29)
+        val createRoomCard: CardView = view.findViewById(R.id.createRoomCard)
+        val joinRoomCard: CardView = view.findViewById(R.id.joinRoomCard)
+        createTextView.isClickable = false
+        joinTextView.isClickable = true
+        createRoomCard.visibility = View.VISIBLE
+        joinRoomCard.visibility = View.GONE
+    }
+
+    private fun basicJoin(view: View, switch: Int){
+        val createTextView: TextView = view.findViewById(R.id.textView28)
+        val joinTextView: TextView = view.findViewById(R.id.textView29)
+        val createRoomCard: CardView = view.findViewById(R.id.createRoomCard)
+        val joinRoomCard: CardView = view.findViewById(R.id.joinRoomCard)
+        val enterRoomCode: EditText = view.findViewById(R.id.enterRoomCode)
+        val joinBt: Button = view.findViewById(R.id.joinBt)
+        val leaveBt: Button = view.findViewById(R.id.leaveBt)
+        val roomCode: TextView = view.findViewById(R.id.roomCode)
+        createTextView.isClickable = true
+        joinTextView.isClickable = false
+        createRoomCard.visibility = View.GONE
+        joinRoomCard.visibility = View.VISIBLE
+
+        if(switch==0)
+        {
+            enterRoomCode.visibility = View.VISIBLE
+            joinBt.visibility = View.VISIBLE
+            leaveBt.visibility = View.GONE
+            roomCode.visibility = View.GONE
+        }else if(switch==1) {
+            //Toast.makeText(requireContext(), "1 was called", Toast.LENGTH_LONG).show()
+            createTextView.isClickable = false
+            createRoomCard.visibility = View.GONE
+            joinRoomCard.visibility = View.VISIBLE
+            enterRoomCode.visibility = View.GONE
+            joinBt.visibility = View.GONE
+            leaveBt.visibility = View.VISIBLE
+            roomCode.visibility = View.VISIBLE
+        }else if(switch==2)
+        {
+            val i: Int = enterRoomCode.text.toString().toInt()
+            store.collection("games").get().addOnSuccessListener {
+                var hostUid: String =""
+                for(s in it)
+                {
+                    val person: HashMap<String, Any> = s.data as HashMap<String, Any>
+                    for(t in person)
+                    {
+                        if(t.key==i.toString()){
+                            val game: HashMap<String, Any> = t.value as HashMap<String, Any>
+                            val host: HashMap<String, Any> = game["host"] as HashMap<String, Any>
+                            hostUid = host["uid"].toString()
+                        }
+                    }
+                }
+                if(hostUid=="")
+                {
+                    Toast.makeText(requireContext(), "Room not found", Toast.LENGTH_LONG).show()
+                    return@addOnSuccessListener
+                }
+
+                Toast.makeText(requireContext(),"Joining Room", Toast.LENGTH_LONG).show()
+                store.collection("games").document(hostUid).get().addOnSuccessListener {
+                    val gameData: HashMap<String, Any> = it[i.toString()] as HashMap<String, Any>
+                    val otherPlayers: HashMap<String, Any> = gameData["otherPlayers"] as HashMap<String, Any>
+                    if(otherPlayers["no"].toString().toInt()>=gameData["maxPlayers"].toString().toInt() && otherPlayers[uid]==null)
+                    {
+                        Toast.makeText(requireContext(), "Room is already full", Toast.LENGTH_LONG).show()
+                        return@addOnSuccessListener
+                    }
+                    if((gameData["host"] as HashMap<String, Any>)["uid"].toString()==uid)
+                    {
+                        liveUpdateJoin(uid, i,view)
+                        return@addOnSuccessListener
+                    }
+                    if(otherPlayers[uid]==null)
+                        otherPlayers["no"] = otherPlayers["no"].toString().toInt() +1
+                    val myData: HashMap<String, Any> = hashMapOf()
+                    myData["uid"] = uid
+                    store.collection("desc").document(uid).get().addOnSuccessListener {
+                        myData["name"] = it["displayName"].toString()
+                        otherPlayers[uid] = myData
+                        gameData["otherPlayers"] = otherPlayers
+                        val up: HashMap<String, Any> = hashMapOf()
+                        up[i.toString()] = gameData
+                        store.collection("games").document(hostUid).update(up).addOnSuccessListener {
+                            Toast.makeText(requireContext(), "Room Joined",Toast.LENGTH_LONG).show()
+                            store.collection("games").document(uid).get().addOnSuccessListener {
+                                val updateMy: HashMap<String, Any> = it.data as HashMap<String, Any>
+                                updateMy["anyGameActive"] = i
+                                liveUpdateJoin(hostUid, i, view)
+                                store.collection("games").document(uid).update(updateMy)
+                            }
+                        }
+
+                    }
+
+                }
+
+
+            }
+        }
+    }
+
+    private fun liveUpdateJoin(hostUid:String, i:Int, view: View){
+        val player1: TextView = view.findViewById(R.id.player1)
+        val player2: TextView = view.findViewById(R.id.player2)
+        val roomCode: TextView = view.findViewById(R.id.roomCode)
+        val maxPlayers: TextView = view.findViewById(R.id.maxPlayers)
+        val enterRoomCode: EditText = view.findViewById(R.id.enterRoomCode)
+        val leaveBt: Button = view.findViewById(R.id.leaveBt)
+        val joinBt: Button = view.findViewById(R.id.joinBt)
+
+        leaveBt.visibility = View.VISIBLE
+        joinBt.visibility = View.GONE
+        enterRoomCode.visibility = View.GONE
+
+        store.collection("games").document(hostUid).get().addOnSuccessListener {
+            val gameData: HashMap<String, Any> = (it[i.toString()] as HashMap<String, Any>)
+            player1.text = (gameData["host"] as HashMap<String,Any>)["name"].toString()
+
+            val otherPlayers: HashMap<String, Any> = gameData["otherPlayers"] as HashMap<String, Any>
+            var player: String = ""
+            for(p in otherPlayers)
+            {
+                if(p.key!="no")
+                {
+                    val user: HashMap<String, Any> = p.value as HashMap<String, Any>
+                    player+=user["name"].toString()
+                }
+            }
+            player2.text = player
+            roomCode.text = i.toString()
+            val s:String = maxPlayers.text.toString()
+            maxPlayers.text = "Max Players: -" + gameData["maxPlayers"].toString()
+        }
+
+    }
+    private fun masterJoin() {
+
+    }
+
+    private fun masterCreate(){
+
+    }
+
+    private fun leaveRoom(){
+        Toast.makeText(requireContext(), "Leave fun pending", Toast.LENGTH_LONG).show()
+
+    }
+
+    private fun createRoom(view: View, players: String,bets: String) {
 //        store.collection("games").document(uid).get().addOnSuccessListener {
-//            val temp: HashMap<String, Any> = it.data as HashMap<String, Any>
-//            if(temp["i"]==null) {
-//                temp["i"] = 1
-//                store.collection("games").document(uid).update(temp)
+//            if((it["anyGameActive"].toString().toInt())!=-1)
+//            {
+//                Toast.makeText(requireContext(), "Room already active: Error", Toast.LENGTH_LONG).show()
+//                return@addOnSuccessListener
 //            }
-//
-//            val gameMap: HashMap<String, Any> = hashMapOf()
-//
-//            temp[temp["i"].toString()] = gameMap
 //        }
 
         store.collection("gamesCount").document("i").get().addOnSuccessListener {
-            val i:Int = it["i"].toString().toInt()
-            val gamesMap: HashMap<String, Any> = hashMapOf()
-            val temp: HashMap<String, Any> = hashMapOf()
-            val host: ArrayList<String> = arrayListOf()
-
+            val i: Int = it["i"].toString().toInt()
+            val userGames: HashMap<String, Any> = hashMapOf()
+            val myGame: HashMap<String,Any> = hashMapOf()
+            myGame["maxPlayers"] = players
+            myGame["bets"] = bets
+            val aboutHost: HashMap<String, Any> = hashMapOf()
+            val otherPlayers: HashMap<String, Any> = hashMapOf()
+            otherPlayers["no"] = 1
+            aboutHost["uid"] = uid
+            myGame["otherPlayers"] = otherPlayers
+            userGames["anyGameActive"] = i
             store.collection("desc").document(uid).get().addOnSuccessListener {
-                host.add(it["displayName"].toString()); host.add(uid)
-                gamesMap["host"] = host
-                gamesMap["maxP"] = a.toInt()
-                gamesMap["bets"] = bets.toInt()
-                val otherPlayers: ArrayList<ArrayList<String>> = arrayListOf()
-                gamesMap["otherPlayers"] = otherPlayers
-                gamesMap["isActive"] = true
-                temp["anyGameActive"] = i
-
-                temp[i.toString()] = gamesMap
-                store.collection("games").document(uid).update(temp).addOnSuccessListener {
-                    val count: HashMap<String, Any> = hashMapOf()
-                    count["i"] = i+1
-                    store.collection("gamesCount").document("i").update(count)
-                    Snackbar.make(requireView(), "Room has been created", Snackbar.LENGTH_SHORT).show()
-                    updateJoinCard(view)
-                }.addOnFailureListener{
-                    Snackbar.make(requireView(), "Failed to create room.", Snackbar.LENGTH_SHORT).show()
+                aboutHost["name"] = it["displayName"].toString()
+                myGame["host"] = aboutHost
+                userGames[i.toString()] = myGame
+                store.collection("games").document(uid).update(userGames).addOnSuccessListener {
+                    val newI: HashMap<String, Any> = hashMapOf()
+                    newI["i"] = i+1
+                    store.collection("gamesCount").document("i").update(newI)
+                    Toast.makeText(requireContext(), "Room created", Toast.LENGTH_LONG).show()
+                    basicJoin(view,0)
                 }
             }
         }
     }
-
-
-    fun updateJoinCard(view: View) {
-
-        store.collection("games").document(uid).get().addOnSuccessListener {
-            val p1: TextView = view.findViewById(R.id.player1)
-            val p2: TextView = view.findViewById(R.id.player2)
-//            val p3: TextView = view.findViewById(R.id.player3)
-//            val p4: TextView = view.findViewById(R.id.player4)
-            ref.addSnapshotListener{snapshot, e ->
-                e?.let {
-                    Toast.makeText(requireContext(), "An error occured.", Toast.LENGTH_LONG).show()
-                }
-                snapshot?.let {
-                    val i: Int = it["anyGameActive"].toString().toInt()
-                    val gameS: HashMap<String, Any> = it[i.toString()] as HashMap<String, Any>
-                    p1.text = (gameS["host"] as ArrayList<String>)[0] + " (host)"
-                    val arr: ArrayList<ArrayList<String>?>? = it["otherPlayers"] as ArrayList<ArrayList<String>?>?
-                    var s: String = ""
-
-                    if(arr!=null)
-                    {
-                        for(j in arr)
-                        {
-                            if(j!=null)
-                                s+=j[0]+"\n"
-                        }
-                    }
-
-                    p2.text = s
-                }
-            }
-        }
-    }
-
 }
