41659fff3d1b627d7eda2f89c63916e1ba0fde4e
Sanyam Ratreja <sanyamratreja18@gmail.com>
2022-02-27 00:46:15 +0530

    paste link

--- app/src/main/java/com/example/quizzitch/PasteLinkFragment.kt
+++ app/src/main/java/com/example/quizzitch/PasteLinkFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import java.sql.Time
@@ -28,22 +29,33 @@ class PasteLinkFragment : Fragment() {
         super.onViewCreated(view , savedInstanceState)
 
         val generatebutton: Button = view.findViewById(R.id.generatebutton)
-
         val pastelink : EditText = view.findViewById(R.id.pastelink)
+        val time: EditText = view.findViewById(R.id.editTextTime)
+
+
+
         generatebutton.setOnClickListener {
-            store.collection("quiz").document(uid).get().addOnSuccessListener {
-                val map: HashMap<String,Any> = hashMapOf()
-                if (it["time"]==null){
-                    map["time"] = 0
-                }
-                val time : EditText = view.findViewById(R.id.editTextTime)
-                map["time"] = time.text.toString()
-                store.collection("quiz").document(uid).update(map).addOnSuccessListener {
+            if(pastelink.text.toString().isEmpty())
+            {
+                Toast.makeText(requireContext(), "Please enter link first", Toast.LENGTH_SHORT).show()
+            }else if(time.text.toString().isEmpty())
+            {
+                Toast.makeText(requireContext(), "Please enter timer first", Toast.LENGTH_SHORT).show()
+            }else
+            {
+                store.collection("quiz").document("code").get().addOnSuccessListener {
+                    val roomCode:String = it["i"].toString()
+                    val map: HashMap<String, Any> = hashMapOf()
+                    map["link"] = pastelink.text.toString()
+                    map["time"] = time.text.toString()
+                    store.collection("quiz").document(uid).collection(roomCode).document("gen").set(map).addOnSuccessListener {
+                        Toast.makeText(requireContext(), "Room Generated", Toast.LENGTH_SHORT).show()
+                        val m: HashMap<String, Any> = hashMapOf()
+                        m["i"] = roomCode.toInt()+1
+                        store.collection("quiz").document("code").set(m)
+                    }
                 }
             }
-
-            store.collection("quizCount").document("a").get().addOnSuccessListener {}
-//            val roomCode : String = (it.data as HashMap<String,Any>)["a"].toString()
         }
 
 
