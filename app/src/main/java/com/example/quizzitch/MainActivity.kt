9b35b9ddd97dee55d7ba51d0d3e03bb19f378300
gurleen2002 <gkchanna123@gmail.com>
2022-02-26 20:04:16 +0530

    add to fav-2

--- app/src/main/java/com/example/quizzitch/MainActivity.kt
+++ app/src/main/java/com/example/quizzitch/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.quizzitch
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.MediaController
 import android.widget.Toast
@@ -14,8 +16,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.levelscreen)
-
-
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.side_toolbar,menu)
+        return true
+    }
+
 }
